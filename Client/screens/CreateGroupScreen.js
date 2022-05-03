4509731b2aec3e8d7955824bb3fb59cb04eaa8d5
ADITYA KUMAR <kumar.92@iitj.ac.in>
2022-05-04 01:50:50 +0530

    create group and join group partially complete

--- Client/screens/CreateGroupScreen.js
+++ Client/screens/CreateGroupScreen.js
@@ -1,13 +1,39 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useEffect, useState} from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+import {Input, Button} from 'react-native-elements';
+import axios from 'axios';
 
 
 const CreateGroupScreen = ({navigation}) => {
+
+    const [groupName, setGroupName] = useState('');
+    const createGroup = () =>{
+        navigation.replace('Home');
+    }
     return (
-        <View>
-            <Text>Create Group Screen</Text>
+        <View style={styles.container}>
+            <Input
+                placeholder='Enter Group Name'
+                label='Group Name'
+                leftIcon={{ type: 'material', name: 'badge' }}
+                value={groupName}
+                onChangeText={text => setGroupName(text)}
+            />
+            <Button title="Create Group" style={styles.button} onPress = {createGroup} />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        padding: 10
+    },
+    button: {
+        width: 200,
+        marginTop: 10
+    }
+})
 
 export default CreateGroupScreen;
--- Client/screens/JoinGroupScreen.js
+++ Client/screens/JoinGroupScreen.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+import {Input, Button} from 'react-native-elements';
+import axios from 'axios';
+
 
 const JoinGroupScreen = ({navigation}) => {
+    const [groupCode, setGroupCode] = useState('');
+    const joinGroup = () =>{
+        navigation.replace('Home');
+    }
     return (
-        <View>
-            <Text>Join Group Screen</Text>
+        <View style={styles.container}>
+            <Input
+                placeholder='Enter Group Code'
+                label='Group Code'
+                leftIcon={{ type: 'material', name: 'lock' }}
+                value={groupCode}
+                onChangeText={text => setGroupCode(text)}
+            />
+            <Button title="Join Group" style={styles.button} onPress = {joinGroup} />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        padding: 10
+    },
+    button: {
+        width: 200,
+        marginTop: 10
+    }
+})
 
 export default JoinGroupScreen;
