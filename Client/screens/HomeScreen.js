b86861ed2835a2762a6637bd7e8d0cf34e097752
vyaskartik20 <vyas.2@iitj.ac.in>
2022-05-04 04:25:41 +0530

    ui

--- Client/screens/HomeScreen.js
+++ Client/screens/HomeScreen.js
@@ -5,10 +5,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import {Input, Button} from 'react-native-elements';
 
-const HomeScreen = ({navigation, route})=> {
+const HomeScreen = ({navigation})=> {
 
-  console.log(route.params);
-  const user = route.params.user;
   const [tick,setTick] = useState(0);
 
 
@@ -24,14 +22,6 @@ const HomeScreen = ({navigation, route})=> {
 // //           })
     
 // // }, 5000);
-  var createGrp = () => {
-    navigation.replace('CreateGroup',{user:user});
-  }
-
-  var joinGrp = () => {
-    navigation.replace('JoinGroup',{user:user});
-  }
-
 
   useEffect(() => {
   });
@@ -39,9 +29,27 @@ const HomeScreen = ({navigation, route})=> {
 
   return (
     <View style={styles.container}>
-      <Text>home</Text>
-      <Button title="Create Group" style={styles.button} onPress = {createGrp}></Button>
-      <Button title="Join Group"style={styles.button} onPress = {joinGrp}></Button>
+      <View style={styles.description}>
+        <Text style={styles.descriptionContent1}>
+          Hi Name
+        </Text>
+        <Text style={styles.descriptionContent2}>
+          Your Coordinates : 192.12312, 3124.434324
+        </Text>
+      </View>
+
+      <View style = {styles.options}>
+        <Button title="Create Group" style={styles.button}/>
+        <Button title="Join Group" style={styles.button} />
+      </View>
+
+      <View style={styles.heading}>
+        <Text style={styles.descriptionContent1}>
+          My Groups
+        </Text>
+      </View>
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -76,9 +84,8 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   button: {
-    width: 200,
-    marginTop: 10
-}
+    padding: "5%" 
+  }
 });
 
 export default HomeScreen
